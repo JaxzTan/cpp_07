d1f48357c41e2f6eae7bc2de9f30e4ab01c0cbce
Jaxz Tan <jaxztan@Jaxzs-MacBook-Pro.local>
2025-05-29 12:02:05 +0800

    fix: move templeteinto header file

---
+++ ex00/inc/whatever.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chtan <chtan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 12:00:13 by chtan             #+#    #+#             */
+/*   Updated: 2025/05/29 12:00:29 by chtan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T min(T a, T b) {
+    return (a < b) ? a : b;
+}
+
+template <typename T>
+T max(T a, T b) {
+    return (a > b) ? a : b;
+}
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,30 +6,31 @@
 /*   By: chtan <chtan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 11:53:46 by chtan             #+#    #+#             */
-/*   Updated: 2025/05/29 11:54:53 by chtan            ###   ########.fr       */
+/*   Updated: 2025/05/29 12:01:08 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-template <typename T>
-void swap(T &a, T &b) {
-    T temp = a;
-    a = b;
-    b = temp;
-}
-
-template <typename T>
-T min(T a, T b) {
-    return (a < b) ? a : b;
-}
-
-template <typename T>
-T max(T a, T b) {
-    return (a > b) ? a : b;
-}
+#include "../inc/whatever.hpp"
 
 int main() {
+
+    std::cout << "mandatory part" << std::endl;
+    std::cout << "------------------" << std::endl;
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+    std::cout << std::endl << "bonus part" << std::endl;
+    std::cout << "------------------" << std::endl;
     int x = 5, y = 10;
     std::cout << "Before swap: x = " << x << ", y = " << y << std::endl;
     swap(x, y);
@@ -37,6 +38,5 @@ int main() {
 
     std::cout << "Min of 5 and 10: " << min(5, 10) << std::endl;
     std::cout << "Max of 5 and 10: " << max(5, 10) << std::endl;
-
     return 0;
 }
