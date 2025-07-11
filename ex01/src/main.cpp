4e16c40f6236567f13dfd6b7e7d2786425ab3227
Jaxz Tan <jaxztan@Jaxzs-MacBook-Pro.local>
2025-07-11 12:40:25 +0800

    fix: make funciton pretier and added more test cse

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,18 +6,20 @@
 /*   By: jaxztan <jaxztan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 12:10:03 by chtan             #+#    #+#             */
-/*   Updated: 2025/07/05 15:12:28 by jaxztan          ###   ########.fr       */
+/*   Updated: 2025/07/11 11:45:37 by jaxztan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/iter.hpp"
 
-template <typename T> void print(T &value)
+template <typename T>
+void print(T &value)
 {
     std::cout << value << " ";
 }
 
-template <typename T> void increment(T &value)
+template <typename T>
+void increment(T &value)
 {
     value += 5;
 }
@@ -41,5 +43,14 @@ int main()
 	iter(strs, 3, print<std::string>);
 	std::cout << std::endl;
 
+	double d_arr[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	std::cout << "Original double array: ";
+	iter(d_arr, 5, print<double>);
+	std::cout << std::endl;
+
+	iter(d_arr, 5, increment<double>);
+	std::cout << "Incremented double array: ";
+	iter(d_arr, 5, print<double>);
+	std::cout << std::endl;
 	return 0;
 }
